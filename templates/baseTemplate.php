cf45b0cc3fa74d581f25d69bec7f0c527ab5df73
Karrmax <jules.grivot.pelisson@gmail.com>
2021-03-20 20:15:57 +0100

    created nav.css and nav2.css

--- templates/baseTemplate.php
+++ templates/baseTemplate.php
@@ -6,6 +6,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title><?= $title ?></title>
         <link href="../public/css/<?= $css ?>" rel="stylesheet" />
+        <link href="../public/css/nav2.css" rel="stylesheet" />
+        <link rel="icon" href="../assets/logo.png">
     </head>
         
     <body>
