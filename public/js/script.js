0571369f4b1e6e081eafd6009b785420fe22972b
Karrmax <jules.grivot.pelisson@gmail.com>
2021-03-23 23:39:53 +0100

    created nav2.css and made it responsive

--- public/js/script.js
+++ public/js/script.js
@@ -37,60 +37,60 @@ function register() {
             for (var i = 0; i < data.length; ++i) {
                 switch (data[i]) {
                     case 0:
-                        document.getElementById('errs').innerHTML += '<div>Your account has been created!</div><div>Please validate your email by checking your inbox for a validation link before logging in.</div>';
+                        document.getElementById('errs').innerHTML += '<div>Compte crée</div>';
                         document.getElementById('registerForm').reset();
                         break;
                     case 1:
-                        document.getElementById('errs').innerHTML += '<div class="err">Invalid name entered. (only use letters, spaces, and hyphens)</div>';
+                        document.getElementById('errs').innerHTML += '<div class="err">Nom invalide. (les lettres, les espaces ou les traits-d\'union sont acceptés)</div>';
                         break;
                     case 2:
-                        document.getElementById('errs').innerHTML += '<div class="err">Invalid email entered.</div>';
+                        document.getElementById('errs').innerHTML += '<div class="err">Email invalide</div>';
                         break;
                     case 3:
-                        document.getElementById('errs').innerHTML += '<div class="err">Email does not exist. (This domain does not have a mail server)</div>';
+                        document.getElementById('errs').innerHTML += '<div class="err">Email non-existant (ce nom de domain n\'a pas de serveur mail)</div>';
                         break;
                     case 4:
-                        document.getElementById('errs').innerHTML += '<div class="err">Password must contain: <ul><li>At least 8 characters</li><li>At least one lower case letter</li><li>At least one upper case letter</li><li>At least one number</li><li>At least one special character (~?!@#$%^&*)</li></ul></div>';
+                        document.getElementById('errs').innerHTML += '<div class="err">le mot de passe doit contennir: <ul><li>au moins 8 caractères</li><li>au moins une lettre en minuscule</li><li>au moins une lettre en majuscule</li><li>au moins 1 nombre</li><li>au moins 1 caractere special (~?!@#$%^&*)</li></ul></div>';
                         break;
                     case 5:
-                        document.getElementById('errs').innerHTML += '<div class="err">Passwords do not match. Please re-enter your confirmed password.</div>';
+                        document.getElementById('errs').innerHTML += '<div class="err">les mot de passe ne correspondent pas. veuillez le re-entrer.</div>';
                         break;
                     case 6:
-                        document.getElementById('errs').innerHTML += '<div class="err">Failed to add account to database. Please try again later.</div>';
+                        document.getElementById('errs').innerHTML += '<div class="err">le compte n\'a pas pu être crée. veuillez ressayez plus tard.</div>';
                         break;
                     case 7:
-                        document.getElementById('errs').innerHTML += '<div class="err">An account with this email already exists</div>';
+                        document.getElementById('errs').innerHTML += '<div class="err">Cet email est deja utiliser pour un auutre compte</div>';
                         break;
                     case 8:
-                        document.getElementById('errs').innerHTML += '<div class="err">Failed to connect to the database. Please try again later.</div>';
+                        document.getElementById('errs').innerHTML += '<div class="err">le client n\'a pas pu se connecter a la base de donnée. Veuillez ressayez plus tard.</div>';
                         break;
-                    case 9:
-                        document.getElementById('errs').innerHTML += '<div class="err">Invalid CSRF Token. Please try again later.</div>';
-                        break;
-                    case 10:
-                        document.getElementById('errs').innerHTML += '<div class="err">Failed to send email. Please try again later.</div>';
-                        break;
-                    case 11:
-                        document.getElementById('errs').innerHTML += '<div class="err">Failed to insert request into database. Please try again later.</div>';
-                        break;
-                    case 12:
-                        document.getElementById('errs').innerHTML += '<div class="err">You have excedded your number of allowed validation requests per day</div>';
-                        break;
-                    case 13:
-                        document.getElementById('errs').innerHTML += '<div class="err">The user with this email is already validated</div>';
-                        break;
-                    case 14:
-                        document.getElementById('errs').innerHTML += '<div class="err">A user with this email does not exist</div>';
-                        break;
-                    case 15:
-                        document.getElementById('errs').innerHTML += '<div class="err">Failed to connect to database. Please try again later.</div>';
-                        break;
+                        // case 9:
+                        //     document.getElementById('errs').innerHTML += '<div class="err">Invalid CSRF Token. Please try again later.</div>';
+                        //     break;
+                        // case 10:
+                        //     document.getElementById('errs').innerHTML += '<div class="err">Failed to send email. Please try again later.</div>';
+                        //     break;
+                        // case 11:
+                        //     document.getElementById('errs').innerHTML += '<div class="err">Failed to insert request into database. Please try again later.</div>';
+                        //     break;
+                        // case 12:
+                        //     document.getElementById('errs').innerHTML += '<div class="err">You have excedded your number of allowed validation requests per day</div>';
+                        //     break;
+                        // case 13:
+                        //     document.getElementById('errs').innerHTML += '<div class="err">The user with this email is already validated</div>';
+                        //     break;
+                        // case 14:
+                        //     document.getElementById('errs').innerHTML += '<div class="err">A user with this email does not exist</div>';
+                        //     break;
+                        // case 15:
+                        //     document.getElementById('errs').innerHTML += '<div class="err">Failed to connect to database. Please try again later.</div>';
+                        //     break;
                     default:
-                        document.getElementById('errs').innerHTML += '<div class="err">An unknown error occured. Please try again later.</div>';
+                        document.getElementById('errs').innerHTML += '<div class="err">une erreur s\'est produite. Please try again later.</div>';
                 }
             }
         } catch (e) {
-            document.getElementById('errs').innerHTML = '<div class="err">An unknown error occured. Please try again later.</div>';
+            document.getElementById('errs').innerHTML = '<div class="err">An unknown error occured. Veuillez ressayez plus tard.</div>';
         }
         setTimeout(function() {
             document.getElementById('errs').style.transition = transition;
--- templates/nav.php
+++ templates/nav.php
@@ -1,10 +1,15 @@
 <?php ob_start(); ?>
+<div class="leftSiide">
+    <img class="logo" src="../assets/Logorond2.png" alt="logo" height="50px">
+    <h1>Mesréparations.com</h1>
+</div>
 <nav>
-    <ul>
+    <ul class="nav_links">
         <li><a href="../view/home_page">Home</a></li>
         <li><a href="../view/create_professional_account">Je suis un professionnel</a></li>
         <li><a href="../view/log_in">se connecter</a></li>
         <li><a href="../view/create_account">créer un compte</a></li>
     </ul>
 </nav>
+<!-- <a href="">Je suis artisan</a> -->
 <?php $nav = ob_get_clean(); ?>
