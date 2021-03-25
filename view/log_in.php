9196b06d8626ecd2f9d10f654ed055ccd99c43ad
Karrmax <jules.grivot.pelisson@gmail.com>
2021-03-25 11:46:32 +0100

    created css of login page

--- view/log_in.php
+++ view/log_in.php
@@ -6,14 +6,28 @@
 <header>
     <?=$nav?>
 </header>
-<div id="login_form">
+<div class="login_form">
     <h1>Se connecter</h1>
     <form action="" method="post">
+
+        <div class="txtfield">
+            <input class="" type="text" name="username" required>
+            <span></span>
+            <label for="">Nom d'utilisateur</label>
+        </div>
+
+        <div class="txtfield">
+            <input type="password" name="password" required>
+            <span></span>
+            <label for="">Mot de passe</label>
+        </div>
+
+        <div class="pass">Mot de passe oublié ?</div>
+        <input type="submit" value="Connexion">
         
-        <input class="" type="text" name="username" placeholder="Nom d'utilisateur">
-        <input type="password" name="password" placeholder="Mot de passe">
-        <input type="submit" value="connexion">
-            
+        <div class="singupLink">
+            Pas encore de compte ? </br><a href="create_account">créer mon compte</a>
+        </div>
     </form>
 </div>
 <?php $content = ob_get_clean(); ?>
