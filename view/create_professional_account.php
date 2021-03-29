c2aec8d0403c4c441aba8d02e856f40002adecb4
zAFG57 <73142098+zAFG57@users.noreply.github.com>
2021-03-29 19:24:15 +0200

    Update create_professional_account.php

--- view/create_professional_account.php
+++ view/create_professional_account.php
@@ -6,18 +6,46 @@
     <header>
         <?=$nav?>
     </header>
-    <form action="" method="post">
-    
-        <input type="text" name="username" placeholder="Nom d'utilisateur">
-        
-        <input type="password" name="password" placeholder="Mot de passe">
-        <input type="password" name="passwordVerify" placeholder="Mot de passe">
 
-        <input type="email" name="password" placeholder="email">
+    <div class="registerproForm"> 
+        <h1>Créer un compte professionnelle</h1>
+        <form id="registerproForm"> 
 
-        <input type="submit" value="Créer mon compte">
+            <div class="txtfield">
+                <input type="text" name="username" required autofocus onkeydown="if(event.key === 'Enter'){event.preventDefault();register();}" >
+                <span></span>
+                <label>Nom d'utilisateur</label>
+            </div>
+
+            <div class="txtfield">
+                <input type="text" name="email" required onkeydown="if(event.key === 'Enter'){event.preventDefault();register();}">
+                <span></span>
+                <label>Email</label>
+            </div>
+      
+            <div class="txtfield">
+                <input type="password" name="password" required onkeydown="if(event.key === 'Enter'){event.preventDefault();register();}">
+                <span></span>
+                <label>Mot de passe</label>
+            </div>
+
+            <div class="txtfield">
+                <input type="password" name="passwordVerify" required onkeydown="if(event.key === 'Enter'){event.preventDefault();register();}" >
+                <span></span>
+                <label>Confirmation</label>
+            </div>
+
+            <div id="errs"></div>
         
-    </form>
+            <div class="submitButton" onclick="register();"><p>Créer mon compte</p></div>
+
+            <div class="singinLink">
+                Déjà un compte ? </br><a href="log_in">Se connecter</a> | <a href="email_verification">Vérifier mon email</a>
+            </div>
+        
+
+        </form>
+    </div>
 
 <?php $content = ob_get_clean();?>
 
