445a96cca9e2096ed1ac12feca8a0724427d67f8
strict falcon <73142098+zAFG57@users.noreply.github.com>
2021-04-10 22:25:06 +0200

    Revert "Add files via upload"

--- view/profile_page.php
+++ view/profile_page.php
@@ -13,33 +13,27 @@ if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
     <header>
         <?=$nav?>
     </header>
-    
-    <div class="main">
+    <div class='mainWrapper'>
 
-        <div class="haut">
-            <div class="photo"></div>
-            <h1> $nom du useur </h1>
+<!-- username -->
+        <div class="dispaly_values">
+            <div class="actual_value"><?= 'test'?></div>
+            <div class="modify_Value">test</div>
         </div>
 
-        <div class="bas">
-
-            <div class="email"><h1>$email du user</h1></div>
-            <div class="mdp"><h1>des petit point en mode mdp secret</h1></div>
-
-            <div class="changer">
-                <div class="changeremail"><h1>changer de mdp</h1></div>
-                <div class="changermdp"><h1>changer l'email</h1></div>
-            </div>
-
+<!-- password -->
+        <div class="dispaly_values">
+            <div class="actual_value">test2</div>
+            <div class="modify_Value">test2</div>
         </div>
+
+<!-- email -->
+        <div class="dispaly_values">
+            <div class="actual_value">test3</div>
+            <div class="modify_Value">test3</div>
+        </div>
+
     </div>
-
-
-
-
-
-
-
 <?php $content = ob_get_clean(); ?>
 
 <?php require('../templates/baseTemplate.php'); ?>
