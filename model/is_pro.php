7a159c42919b4cc78ab4bcb715a27b40c11a7c51
Karrmax <jules.grivot.pelisson@gmail.com>
2021-04-11 19:20:33 +0200

    fix isPro.php

--- model/is_pro.php
+++ model/is_pro.php
@@ -4,30 +4,26 @@ session_start();
 
 
 function isPro(){
-    if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true || isset($_SESSION['userID']) || is_int($_SESSION['userID'])) {
-        return false;
-    } else {
+    if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] === true && isset($_SESSION['userID']) && is_int($_SESSION['userID'])) {
+        $db = connect();
         if($db) {
-            $res = sqlSelect($db, 'SELECT users.pro, users.verified FROM users WHERE users.id' ,'i', $_SESSION['userID']);
+            $res = sqlSelect($db, 'SELECT users.pro, users.verified FROM users WHERE users.id = ?' ,'i', $_SESSION['userID']);
 
             if ($res->num_rows === 1) {
                 $userPro = $res->fetch_assoc();
                 
-                if($userPro['users.verified']) {
-                    if($userPro['users.pro'] === 1){
+                if($userPro['verified']) {
+                    if($userPro['pro'] === 1){
                         return true;
-                    } else {
-                        return false;
                     }
-
-                } else {
-                    echo -2;
                 }
-
-            } else {
-                echo -1;
-                return;
             }
+    
         }
     }
+    return false;
 }
+
+// if (condition) {
+    echo isPro();
+// }
