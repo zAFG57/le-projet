217eb36576c1754a31e741050e2c7cce1dfa64ea
Karrmax <62204474+Karrmax@users.noreply.github.com>
2021-04-13 23:28:15 +0200

    Update index.php

--- index.php
+++ index.php
@@ -24,43 +24,6 @@ $authorized_pages = array(
     "emailVerification" => "view/email_verification",
     "homePage" => "view/home_page",
     "profile" => "view/profile_page",
-    "isPro" => "model/is_pro"
-);
-
-if(isset($_GET['location']) && isset($authorized_pages[$_GET['location']])){
-    header("Location: {$authorized_pages[$_GET['location']]}");
-    die();
-} else {
-    header("Location: {$authorized_pages['homePage']}");
-    exit;
-}
-<?php
-session_start();
-
-if(!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
-
-
-    $authorized_pages = array(
-        "createAccount" => "view/create_account",
-        "createProAccount" => "view/create_professional_account",
-        "emailVerification" => "view/email_verification",
-        "login" => "view/log_in",    
-    );
-
-    if(isset($_GET['location']) && isset($authorized_pages[$_GET['location']])){
-        header("Location: {$authorized_pages[$_GET['location']]}");
-        die();
-    } else {
-        header("Location: {$authorized_pages['login']}");
-        exit;
-    }
-}
-
-$authorized_pages = array(
-    "emailVerification" => "view/email_verification",
-    "homePage" => "view/home_page",
-    "profile" => "view/profile_page",
-    "isPro" => "model/is_pro"
 );
 
 if(isset($_GET['location']) && isset($authorized_pages[$_GET['location']])){
