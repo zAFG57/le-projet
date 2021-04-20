ef1823cae012ed8befba6f4547b07580eea4bfb2
Karrmax <jules.grivot.pelisson@gmail.com>
2021-04-20 11:36:34 +0200

    rework chat.php

--- view/chat.php
+++ view/chat.php
@@ -2,40 +2,44 @@
 
     session_start();
 
-include_once('../templates/nav.php');
+    include_once('../templates/nav.php');
 
     require_once('../controller/chatProUser.php');
     require_once('../controller/user.php');
-    // // echo $_SESSION['userID'];
 
-    if (isset($_GET['proID']) && isset($_SESSION['userID'])) {
-        if(ControllerChatProUser::openChat($_SESSION['userID'], intval($_GET['proID'])) > 0){
+
+
+    if (!ControllerUser::isConnected()) {
+        header("Location: ../index.php?location=chat");
+        exit;
+    }
+
+        if(ControllerChatProUser::openChat($_SESSION['userID'], intval($_GET['proID']))){
             header("Location: ./chat.php?chatID=". ControllerChatProUser::openChat($_SESSION['userID'], intval($_GET['proID'])));
             exit;
         } else {
             header("Location: ./chat.php");
             exit;
         }
-    }
-
-    $title = "Chat"; $css = "chat.css";
-    ob_start(); 
 ?> 
+
+<?php 
+    $title = "Chat"; $css = "chat.css";
+    ob_start();  
+?>
 
 <header>
     <?=$nav?>
 </header>
 
 <?php
-    if(!isset($_GET['chatID'])) {
+
+    if(isset($_GET['chatID']) && ControllerChatProUser::displayMessages($_SESSION['userID'], intval($_GET['chatID']))) {
+        require_once('../view/chats/displayChat.php');
+    } else {
         require_once('./chats/chatBase.php');
-    } else if(isset($_GET['chatID'])) {
-        if(ControllerChatProUser::displayMessages($_SESSION['userID'], intval($_GET['chatID']))) {
-            require_once('../view/chats/displayChat.php');
-        } else {
-            require_once('./chats/chatBase.php');
-        }
     }
+    
 ?>
 
 <?php $content = ob_get_clean(); ?>
