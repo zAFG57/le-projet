0ccd6ad025e5c7456ba773ff0cb1d638ff0be160
strict falcon <pediluve.royal2@gmail.com>
2021-06-10 19:44:00 +0200

    la base du merge qui fonctionne (ludo)

--- view/vEmail/email_verification_page.php
+++ view/vEmail/email_verification_page.php
@@ -1,20 +1,24 @@
+<?php 
+    $json = 'verif';
+    require('../templates/lang.php');   
+?>
 <div class="verificationForm">
-    <h1>Vérifier mon Email</h1>
+    <h1><?=$parsed_lang->{'vemail'}?></h1>
 
     <form id="verificationForm">
 
         <div class="txtfield">
             <input type="text" name="validateEmail" required autofocus onkeydown="if(event.key === 'Enter'){event.preventDefault();sendValidateEmailRequest();}" >
             <span></span>
-            <label>Email</label>
+            <label><?=$parsed_lang->{'Email'}?></label>
         </div>
 
         <div id="errs"></div>
         
-        <div class="submitButton" onclick="sendValidateEmailRequest();"><p>Envoyer la vérification</p></div>
+        <div class="submitButton" onclick="sendValidateEmailRequest();"><p><?=$parsed_lang->{'elv'}?></p></div>
 
         <div class="singinLink">
-            Déjà un compte ? </br><a href="log_in">Se connecter</a>
+            <?=$parsed_lang->{'duc'}?></br><a href="log_in"><?=$parsed_lang->{'connect'}?></a>
         </div>
         
 
